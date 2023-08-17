bd61f868813d18b5ec4ee4b208d17df917ab7ca7
Ana Clara Ribeiro Vigas <142412202+anaclaravigas@users.noreply.github.com>
2023-08-17 00:20:51 +0000

    validando a entrada

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,7 +1,54 @@
+/**
+ * Objeto que representa um pedido
+ * @typedef {Object} Pedido
+ * @property {string} metodoDePagamento - Método de pagamento para o pedido.
+ * @property {Array.<string>} itens - Lista de itens no pedido.
+ */
+
+
+const formasDePagamento = {
+    'dinheiro': .95,
+    'credito': 1.03,
+    'debito': 1,
+}
+
+
+
+/**
+ * Calcula o valor da compra
+ * @param {Pedido} pedido
+ */
+function validaEntrada(pedido) {
+    if (pedido.itens.length === 0) {
+        return 'Não há itens no carrinho de compra!'
+    } 
+    if (!formasDePagamento[pedido.metodoDePagamento]) {
+        return 'Forma de pagamento inválida!'
+    }
+    return pedido
+}
+
+
 class CaixaDaLanchonete {
 
+    /**
+     * Calcula o valor da compra
+     * @param {string} metodoDePagamento
+     * @param {Array.<string>} itens - Descrição do item.
+     */
     calcularValorDaCompra(metodoDePagamento, itens) {
-        return "";
+        const pedido = {metodoDePagamento, itens}
+
+        const operacoes = [
+            validaEntrada,
+        ]
+
+        for (const op of operacoes) {
+            const result = op(pedido)
+            if (typeof result === "string") {
+                return result
+            }
+        }
     }
 
 }
